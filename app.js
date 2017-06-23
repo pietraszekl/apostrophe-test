f6634b3186f8205abae779cb90c93405b79e90fe
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2017-06-23 07:20:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var apos = require('apostrophe')({
+  shortName: 'apostrophe-project',
+  title: 'apostrophe-project',
+
+  // See lib/modules for basic project-level configuration of our modules
+  // responsible for serving static assets, managing page templates and
+  // configuring user acounts.
+
+  modules: {
+    // Add custom apostrophe-modules and their respective configuration here!
+  }
+});
